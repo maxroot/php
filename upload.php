ceb7fc6fe917cf8434f5cfba7edb1046d17325d3
Matheus Nasc <webjasn@gmail.com>
2016-12-07 08:39:17 -0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -21,10 +21,10 @@ by m3nstr1x
 <!DOCTYPE html>
 <html>
 <head>
-<title>Upload By M3nstr1x</title>
+<title>Upload By v0ld3x</title>
 </head>
 <body>
- <h1 style="font-family: Courier New; font-size:30px; ">Upload By M3nstr1x</h1><br>
+ <h1 style="font-family: Courier New; font-size:30px; ">Upload By v0ld3x</h1><br>
 <form action="" method="post" enctype="multipart/form-data">
 <input type="file" name="pic" />
 <button type="submit" name="btn-upload">upload</button>
